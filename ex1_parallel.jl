1ae5512d247ee501c2c82543dd9f97d4190cefda
Your Github Id <nobody@nowhere.org>
2025-09-27 17:07:18 -0400

    multithreading option for ex1

--- ex1_parallel.jl
+++ ex1_parallel.jl
@@ -1,5 +1,5 @@
 using Distributed 
-@everywhere using ParallelUtilities
+@everywhere using ParallelUtilities, OhMyThreads
 
 @everywhere function calc_num_in_unit_circle(n::Integer)
    num_in_unit_circle = 0
@@ -13,6 +13,12 @@ using Distributed
    return num_in_unit_circle
 end
 
+
+@everywhere function calc_num_in_unit_circle_threaded(n::Integer)
+    num_threads = Threads.nthreads()
+    chunks = index_chunks(1:n; n = num_threads)
+    tmapreduce(idx->calc_num_in_unit_circle(length(idx)), +, chunks)
+end
 
 function calc_pi_monte_carlo(n_throws::Integer; n_in_circle::Integer = calc_num_in_unit_circle(n))
    4 * (n_in_circle/n_throws)
@@ -31,9 +37,15 @@ end
 n_per_worker = floor(Int64,n//nworkers())
 n_actual = n_per_worker * nworkers()
 # Run once to force compile of needed functions
-calc_pi_monte_carlo(n_actual; n_in_circle = pmapreduce(calc_num_in_unit_circle, +, fill(1, nworkers()) ) ) 
+calc_pi_monte_carlo(n_actual; n_in_circle = pmapreduce(calc_num_in_unit_circle, +, fill(128, nworkers()) ) ) 
+calc_pi_monte_carlo(n_actual; n_in_circle = pmapreduce(calc_num_in_unit_circle_threaded, +, fill(128, nworkers()) ) ) 
 GC.gc()     # Force garbage collection before timing
 
-@time pi_estimate = calc_pi_monte_carlo(n_actual; n_in_circle = pmapreduce(calc_num_in_unit_circle, +, fill(n_per_worker, nworkers()) ) ) 
+if Threads.nthreads() == 1
+   @time pi_estimate = calc_pi_monte_carlo(n_actual; n_in_circle = pmapreduce(calc_num_in_unit_circle, +, fill(n_per_worker, nworkers()) ) ) 
+else
+   @time pi_estimate = calc_pi_monte_carlo(n_actual; n_in_circle = pmapreduce(calc_num_in_unit_circle_threaded, +, fill(n_per_worker, nworkers()) ) ) 
+end
+
 println("# After ", n_per_worker, " itterations on each of ", nworkers(), " workers, estimated pi to be...")
 println(pi_estimate)
