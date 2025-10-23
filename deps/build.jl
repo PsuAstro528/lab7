c174f1f2e62ec85d290a5658a739716e1c26fd25
Your Github Id <nobody@nowhere.org>
2025-10-22 23:03:31 -0400

    fix build scr

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,6 @@
 import Pkg
 
 Pkg.UPDATED_REGISTRY_THIS_SESSION[] = true   # Tell Julia package manager not to download an updated registry for
-Pkg.activate(".")
+#Pkg.activate(".")
 Pkg.instantiate()  # Not needed if we've already installed packages into this project when we prepared the inputs
 Pkg.precompile()   # Make sure packagers are compiled in serial before starting parallel processing.
